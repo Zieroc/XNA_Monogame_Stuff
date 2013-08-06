94e7f460bdb934685b72b3e117d735953ce6fda8
Christy Carroll <ChristyCarroll1@gmail.com>
2013-08-06 21:27:14 +0100

    Deleting some files

--- Snippets/TurretFaceTarget.cs
+++
@@ -1,9 +0,0 @@
-//Epic Awesome Tank - Enemy Turret Face Player
-protected override void RotateTurret()
-{
-	//Calculate angle between tank and player
-	double deltaY = target.Position.Y - turret.Position.Y;
-	double deltaX = target.Position.X - turret.Position.X;
-	double angleDegrees = Math.Atan2(deltaY, deltaX) * 180 / MathHelper.Pi;
-	turretRotation = (float)Math.Atan2(deltaX, -deltaY);
-}
